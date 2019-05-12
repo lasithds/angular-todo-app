e5cea517f98e4956b1b09af415e12bb8ac1695e5
Lasith De Silva <lasithds@live.com>
2019-05-12 12:48:34 +0530

    todo app created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodoDataService } from './todo-data.service';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-app';
+  newTodo: Todo = new Todo();
+
+  constructor(private todoDataService: TodoDataService) {
+
+  }
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  completedTodos(){
+    return this.todos.filter(todo => todo.complete).length;
+  }
+
+   // Service is now available as this.todoDataService
+  toggleTodoComplete(todo: Todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+  removeTodo(todo: Todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos(): Todo[] {
+    return this.todoDataService.getAllTodos();
+  }
+
 }
---
+++ src/app/todo-data.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './todo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoDataService {
+
+  lastId = 0;
+
+  todos: Todo[] = [];
+
+  constructor() { }
+
+  getAllTodos(): Todo[] {
+    return this.todos;
+  }
+
+  addTodo(todo: Todo): TodoDataService {
+    if (!todo.id) {
+      todo.id = ++this.lastId;
+    }
+    this.todos.push(todo);
+    return this;
+  }
+
+  deleteTodoById(id: number): TodoDataService {
+    this.todos = this.todos.filter(todo => todo.id !== id);
+    return this;
+  }
+
+  getTodoById(id: number): Todo {
+    return this.todos.filter(todo => todo.id === id).pop();
+  }
+
+  updateTodoById(id: number, values: object = {}): Todo {
+    const todo = this.getTodoById(id);
+    if (!todo) {
+      return null;
+    }
+    Object.assign(todo, values);
+    return todo;
+  }
+
+  toggleTodoComplete(todo: Todo): Todo {
+    const updatedTodo = this.updateTodoById(todo.id, {
+      complete: !todo.complete
+    });
+    return updatedTodo;
+  }
+}
